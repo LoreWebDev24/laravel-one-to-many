788a13e91f7596c922ed2424201259e4c66207c1
Lorenzo Del Re <136103234+LoreWebDev24@users.noreply.github.com>
2023-12-21 16:02:28 +0100

    STARTED- Projects Views

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,13 @@
+
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        return view('admin.dashboard');
+    }
+}
