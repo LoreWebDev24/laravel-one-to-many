f9006ee4120243c3de4c9a3aa4503216590cbbb5
Lorenzo Del Re <136103234+LoreWebDev24@users.noreply.github.com>
2023-12-21 16:39:58 +0100

    ADD - Project index view, resource is now displayed

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <section>
+        <div class="container pt-5">
+            <h1 class="text-center pb-5">Your Projects</h1>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Slug</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($projects as $project)
+                        <tr>
+                            <td>{{ $project->id }}</td>
+                            <td>
+                                <a href="{{ route('admin.projects.show', $project) }}">
+                                    {{ $project->title }}
+                                </a>
+                            </td>
+                            <td>{{ $project->slug }}</td>
+                            <td>
+                                <a href="{{ route('admin.projects.edit', $project) }}">Edit</a>
+                            </td>
+                            <td>
+                                <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <input class="btn btn-danger btn-sm" type="submit" value="Delete">
+                                </form>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>No Projects to Show</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </section>
+@endsection
